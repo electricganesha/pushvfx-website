7bf73d5fd2d81f8d43ad25dff1f87aedfd25d646
Unknown <ricardoadspinto@gmail.com>
2017-02-24 16:37:07 +0000

    Reel 40%

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ var appClientFiles = [
   'app_client/home/home.controller.js',
   'app_client/portfolio/portfolio.controller.js',
   'app_client/team/team.controller.js',
+  'app_client/reel/reel.controller.js',
   'app_client/common/directives/pageHeader/pageHeader.directive.js',
   'app_client/common/directives/pageFooter/pageFooter.directive.js',
   'app_client/common/services/getData.service.js'
