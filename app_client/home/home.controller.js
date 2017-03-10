ff54e0dd0280dc9f186f798b570975bcf711c317
Unknown <ricardoadspinto@gmail.com>
2017-03-10 18:36:47 +0000

    Media querys 80%

--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -7,6 +7,32 @@
   homeCtrl.$inject = ['$scope', 'getData', 'saveNewsId'];
   function homeCtrl ($scope, getData, saveNewsId) {
 
+    var lastScrollTop = 0;
+    var up = false;
+    var viewportWidth = $(window).width();
+    console.log(viewportWidth);
+    
+    if(viewportWidth > 1000){
+      $(window).scroll(function(event){
+        if($(this).scrollTop()){
+          var st = $(this).scrollTop();
+          if (st > lastScrollTop){
+            if(up == false){
+              //$("#cabecalho").animate({top: "-220px"}, 'fast', 'swing');
+              $("#cabecalho").animate({top: "-58px"});
+              up = true;
+            }
+          } else {
+              if(up == true){
+                $("#cabecalho").animate({top: "0px"}, 'fast','swing');
+                up = false;
+              }
+          }
+          lastScrollTop = st;
+        }
+      });
+    }
+
     getData.latestwork()
       .then(function (data){
         $scope.bestprojects = data;
